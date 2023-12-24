65c04a2c970987baa8f21eaf78a15de349462384
Sambhav Mahajan <sambhavmahajan123@gmail.com>
2023-12-24 12:07:06 +0530

    ApplicationConfiguration.Initialize(); written outside of if statement in program.cs, fixed drag and drop file on exe to open it bug(it did not work before).

--- FrustraType/Program.cs
+++ FrustraType/Program.cs
@@ -8,16 +8,16 @@ namespace FrustraType
         [STAThread]
         static void Main(string[] args)
         {
+            ApplicationConfiguration.Initialize();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             if (args.Length == 0)
             {
-                ApplicationConfiguration.Initialize();
                 Application.Run(new Form1());
             }else
             {
                 foreach(string s in args) {
-                    ApplicationConfiguration.Initialize();
+                    
                     Application.Run(new Form1(s));
                 }
             }
